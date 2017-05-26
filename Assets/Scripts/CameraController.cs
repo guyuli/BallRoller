b1048052a711300d222b955a4ad8435ff70a9a11
Rain_Li <36seed@gmail.com>
2017-05-26 03:01:53 -0400

    commit to master

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset; // original distance between camera and player(sphere)
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	
+	}
+	
+	// LateUpdate is called once per frame, but guaranteed to run after all items have been processed
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Ball Roller
-a simple rolling ball game
+
+A simple rolling ball game developed using C# on Unity3D
+
+Goal:
+Try to collect all yellow cubes!
+
+Steps to play:
+1. Navigate to "Builds" folder
+2. Double click on "BallRoller (Mac)"
